8e14a2096d26169128fdf342614fde0e00fbbd57
Aliyu Tunde <aliumuhammad2000@gmail.com>
2026-03-03 07:36:39 -0800

    created a file to save my projects displaying projects on the page using the DOM

---
+++ data/projects.js
@@ -0,0 +1,15 @@
+export const projects = [
+    {
+        Image : "./images/my-store.png",
+        name : "Thundey Store",
+        description : "An e-commerce website for all your shopping needs. Explore a wide range of products and enjoy a seamless shopping experience.",
+        github : "https://github.com/aliumuhammad2000-dev/my-store",
+        liveDemo : "https://thundeystore.netlify.app/"
+    }, {
+        Image : "./images/laptop-ro.png",
+        name : "Royalty Parfait",
+        description : "Discover and order your favourite parfait. For Royalties!",
+        github : "#",
+        liveDemo : "#"
+    }
+]
